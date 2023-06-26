7eded10d28e7bded9f55558ebf9ad73ab34b855a
Thom Zwagers <thomzwagers@gmail.com>
2023-06-26 14:42:20 +0200

    Refactoring and command line args

--- hotncold.py
+++ hotncold.py
@@ -1,26 +1,46 @@
-import pandas as pd
-import requests
-import zipfile
+''' This script calculates the average maximum and minimum temperatures for 
+each day of the week.
+
+usage: python3 station_id
+
+Valid station ids can be found on
+https://www.knmi.nl/nederland-nu/klimatologie/daggegevens
+
+Default value = 260 (De Bilt, which is the reference station in NL)
+
+June 26, 2023, by Thom Zwagers
+'''
+
 import os
+import sys
+import requests
+import pandas as pd
+import zipfile
+
+# read argument from command line, 2nd arg = station id, default = 260
+try:
+    station_id = sys.argv[1]
+except IndexError:
+    station_id = 260
 
 # Download data
 URI = 'https://cdn.knmi.nl/knmi/map/page/klimatologie/gegevens/daggegevens/'
-FILE = 'etmgeg_260.zip'
-DATABESTAND = 'etmgeg_260.txt'
+bestand = f"etmgeg_{station_id}.zip"
+databestand = f"etmgeg_{station_id}.txt"
 
 try:
-    response = requests.get(URI + FILE)
+    response = requests.get(URI + bestand)
     response.raise_for_status()
     
-    with open(FILE, 'wb') as out_file:
+    with open(bestand, 'wb') as out_file:
         out_file.write(response.content)
     
     # Unzip downloaded data
-    with zipfile.ZipFile(FILE, 'r') as zip_ref:
+    with zipfile.ZipFile(bestand, 'r') as zip_ref:
         zip_ref.extractall()
         
     # Delete the zip file after extraction
-    os.remove(FILE)
+    os.remove(bestand)
     
 except requests.exceptions.RequestException as e:
     print(f"Error occurred while downloading the file: {e}")
@@ -29,7 +49,7 @@ except zipfile.BadZipFile as e:
     print(f"Error occurred while extracting the zip file: {e}")
 
 # Read data, only cols DTG and TG to pandas dataframe
-df = pd.read_csv(DATABESTAND, header=50, skip_blank_lines=True,
+df = pd.read_csv(databestand, header=50, skip_blank_lines=True,
                  skipinitialspace=True, usecols=['YYYYMMDD', 'TG', 'TX', 'TN'])
 
 # Create extra column with weekday (0 = Monday, 1 = Tuesday, ..., 6 = Sunday)
